8a3a37206731e844bb7f72302bd2e7df40a5b6df
Melissa Armand <melissarmand@gmail.com>
2021-08-27 19:01:26 -0400

    started the db.js file

---
+++ public/db.js
@@ -0,0 +1,29 @@
+let db;
+let workoutVersion;
+
+const request = indexedDB.open('WorkoutDB', workoutVersion || 21);
+
+request.onupgradeneeded = function (e) {
+    console.log('Upgrade needed in IndexDB');
+
+    const { oldVersion } = e;
+    const nerVersion = e/newVersion || db.nerVersion;
+
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+    db = e.targer.result;
+
+    if (db.objectWorkoutNames.length === 0) {
+        db.createWorkout('Workout', { autoIncrement: true});
+    }
+};
+
+request.onerror = function (e) {
+    console.log(`Woops! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+    console.log('check db invoked');
+
+    let workout = db.workout(['Workout'], 'readWrite');
+}
