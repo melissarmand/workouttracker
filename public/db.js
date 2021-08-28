d2adcbe31bc8c7a90ab98692448c75d7d3f25359
Melissa Armand <melissarmand@gmail.com>
2021-08-28 18:51:38 -0400

    finished db.js file

--- public/db.js
+++ public/db.js
@@ -23,7 +23,7 @@ request.onupgradeneeded = function (e) {
 
 // Create a new db request for an exercise database
 
-request.onupgradeneeded = function (e) {
+requestExercise.onupgradeneeded = function (e) {
     console.log('Upgrade needed in IndexDB');
 
     const { oldExercise } = e;
@@ -47,4 +47,95 @@ function checkDatabase() {
 
     let workout = db.workout(['Workout'], 'readWrite');
     let exercise = db.exercise(['Exercise'], 'readWrite');
+
+    const workoutObject = workout.objectworkoutObject('Workout');
+    const exerciseObject = exercise.objectworkoutObject('Exercise');
+
+    const getAllWorkout = workoutObject.getAll();
+    const getAllExercise = exerciseObject.getAll();
+
+    getAllWorkout.onsuccess = function () {
+
+        if (getAllWorkout.result.length > 0) {
+            fetch('/api/workout/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAllWorkout.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    workout = db.workout(['Workout'], 'readWrite');
+
+                    const currentWorkout = workout.objectworkoutObject('Workout');
+
+                    currentWorkout.clear();
+                    console.log('Clearing workout')
+                }
+            })
+        }
+    }
+
+    getAllExercise.onsuccess = function () {
+
+        if (getAllExercise.result.length > 0) {
+            fetch('/api/exercise/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAllExercise.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    workout = db.exercise(['Exercise'], 'readWrite');
+
+                    const currentExercise = exercise.objectexerciseObject('Exercise');
+
+                    currentExercise.clear();
+                    console.log('Clearing exercise')
+                }
+            })
+        }
+    }
 }
+
+request.onsuccess = function (e) {
+    console.log('success');
+    db = e.target.result;
+
+    if(navigator.onLine) {
+        console.log('Backend online! ')
+        checkDatabase();
+    }
+};
+
+ requestExercise.onsuccess = function (e) {
+    console.log('success');
+    db = e.target.result;
+
+    if(navigator.onLine) {
+        console.log('Backend online! ')
+        checkDatabase();
+    }
+ };
+
+ const saveRecord = (record) => {
+     console.log('Save record invoked');
+
+     const workoutObject = workout.objectworkoutObject('Workout');
+     const exerciseObject = exercise.objectworkoutObject('Exercise');
+
+     workoutObject.add(record);
+     exerciseObject.add(record);
+
+ };
+
+ window.addEventListener('online', checkDatabase);
+
+
