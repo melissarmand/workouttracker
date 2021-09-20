3372e79e6b140269f00b5ba5340ff37a9af7c359
Melissa Armand <melissarmand@gmail.com>
2021-09-20 18:48:23 -0400

    updated server.js

---
+++ models/exercise.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    name: {
+        type: String,
+        trim: true,
+        duration: Number,
+        weight: Number, 
+        reps: Number,
+        sets: Number,
+        required: "Please enter all request information"
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
