33528dd582efb983f707ece74bb852f6b55f7b67
Melissa Armand <melissarmand@gmail.com>
2021-09-22 23:07:09 -0400

    added changes to the workout.js file, api.js file in route folder, and seed.js file

--- models/workout.js
+++ models/workout.js
@@ -1,24 +1,49 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
-    exercises: {
-        type: String,
-        trim: true,
-        duration: Number,
-        weight: Number, 
-        reps: Number,
-        sets: Number,
-        required: "Please enter all requested information"
-    },
-
-    date: {
+const WorkoutSchema = new Schema ({
+    
+    day: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [
+        
+        { 
+        type: {
+            type: String,
+            trim: true
+        },
+        name: {
+            type: String,
+            trim: true
+        },
+        duration: {
+            type: Number,
+            trim: true
+        },
+        weight: {
+            type: Number,
+            trim: true
+        },
+        reps: {
+            type: Number,
+            trim: true
+        },
+        sets: {
+            type: Number,
+            trim: true
+        }, 
+        distance: {
+            type: Number,
+            trim: true
+        }
+
+    }],
+
+  
 });
 
-const Workout = mongoose.model("workout", workoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
