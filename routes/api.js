1dd421281ecb4ddaf03570ec1b93f529080cefd9
melissarmand <82781751+melissarmand@users.noreply.github.com>
2021-09-26 02:53:02 -0400

    working on the routes (almost there!)

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 
+
 router.get ("/api/workouts", (req, res) => {
     Workout.find()
     .then(data => {
@@ -9,19 +10,29 @@ router.get ("/api/workouts", (req, res) => {
     })
     .catch(err => {
         res.json(err)
-    })
+    });
 });
 
 
 
-router.post("/api/workout", (req, res) => {
-    Workout.create(req.body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(data => 
+        res.json(data))
     .catch(err => {
         res.status(400).json(err);
-    })
+    });
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, 
+        push({exercises: req.body}))
+        .then(data => 
+            res.json(data))
+            .catch(err => {
+                res.status(400).json(err);
+            });
+
 });
 
 
